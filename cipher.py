99932a03be77fb854138b29c6ec1ffba56588b57
ivyownn <142464742+ivyownn@users.noreply.github.com>
2023-11-09 02:00:00 +0000

    cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,14 @@
 # add your code here
+sentence = input("Please enter your sentence:").lower().strip()
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+shift = 5
+result = ""
+for letter in sentence:
+    if not letter.isalpha():
+        result += letter
+        continue
+    
+    index = alphabet.find(letter)
+    result += alphabet[(index + shift)%26]
+print("The encrypted sentence is:", result)
+   
